528ea9a37de15a723d306ede6943676e8d75b3a3
Velasquez <144332594+almiravelas@users.noreply.github.com>
2025-09-29 12:36:44 +0800

    Update transform.py

--- src/transform.py
+++ src/transform.py
@@ -31,6 +31,9 @@ def transform_dim_rider(df: pd.DataFrame) -> pd.DataFrame:
     df.rename(columns={'id': 'rider_key'}, inplace=True)
     df['gender'] = standardize_gender(df['gender'])
     
+    # typo FEDEZ to FEDEX
+    df['courier_name'] = df['courier_name'].str.replace('FEDEZ', 'FEDEX')
+    
     dim_df = df[['rider_key', 'rider_name', 'vehicleType', 'gender', 'age', 'courier_name']]
     
     print("Rider dimension transformed.")
