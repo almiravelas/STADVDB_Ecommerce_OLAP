df04882be1979cc34e686be04342a7eb225cafa6
Velasquez <144332594+almiravelas@users.noreply.github.com>
2025-10-07 21:25:02 +0800

    Imported app files & wip rider analysis

---
+++ app/queries/user_queries.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import streamlit as st
+from sqlalchemy.engine import Engine
+
+@st.cache_data
+def get_user_data(_engine: Engine) -> pd.DataFrame:
+    if  pd.DataFrame is None:
+        return None
+    pass
+    return pd.DataFrame
---
+++ app/views/date_view.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def show_date_view():
+    st.header("Date Dimension")
+    st.info("WALA PAAAA")
